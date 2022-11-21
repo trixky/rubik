ad7cc5a978ade4e3472a56144192ad88220f0a4f
Mathis Bois <mathis.bois@outlook.fr>
2022-11-21 11:38:11 +0100

    [Dev] about: update the about section

--- client/src/components/layout/footer.svelte
+++ client/src/components/layout/footer.svelte
@@ -1,33 +1,7 @@
 <!-- ========================= HTML -->
 <footer>
 	<p>
-		G0 -> G1
-		<br/>
-		orient edges
-		<br/>
-		<br/>
-		G1 -> G2
-		<br/>
-		orient corners
-		<br/>
-		put FUDB edges in their slice
-		<br/>
-		<br/>
-		G2 -> G3
-		<br/>
-		place corners
-		<br/>
-		put other edges in their slice
-		<br/>
-		<br/>
-		G3 -> G4
-		<br/>
-		place edges
-		<br/>
-		<br/>
-		Each group keeps the previous group rules
-		<br/>
-		by reducing the allowed moves.
+		Using the <a href="https://en.wikipedia.org/wiki/Morwen_Thistlethwaite" target="_blank">Morwen Thistlethwaite</a> and <a href="https://en.wikipedia.org/wiki/Iterative_deepening_A*" target="_blank">IDA*</a> algorithms, rendered with the <a href="https://threejs.org/" target="_blank">Three.js</a> browser <a href="https://en.wikipedia.org/wiki/WebGL" target="_blank">WebGL</a> engine.
 	</p>
 </footer>
 
@@ -35,5 +9,9 @@
 <style lang="postcss">
 	footer {
 		@apply w-[350px] m-auto text-center pt-2 pb-10;
+	}
+
+	a {
+		text-decoration: none;
 	}
 </style>
